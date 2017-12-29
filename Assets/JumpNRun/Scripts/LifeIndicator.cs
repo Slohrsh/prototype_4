66d975d41b4ddd7052d0693753fbb04a1109d3c3
Slohrshi <sebastian.lohr@hs-weingarten.de>
2017-12-29 11:21:11 +0100

    3d Canvas angefangen

---
+++ Assets/JumpNRun/Scripts/LifeIndicator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeIndicator : MonoBehaviour {
+
+    public Canvas Reference;
+    public GameObject LifeIndicatorPrefab;
+    private GameObject lifeIndicator;
+    // Use this for initialization
+    void Start () {
+        lifeIndicator = Instantiate(LifeIndicatorPrefab, Reference.transform);
+        lifeIndicator.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeIndicator.transform.position = this.transform.position + new Vector3(0f,3f);
+	}
+}
